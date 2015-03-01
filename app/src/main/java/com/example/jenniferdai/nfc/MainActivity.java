d16a82db3b0c155ccaf7eb70d80a1a615214dabb
Jennifer Dai <jennifah.hai.dai@gmail.com>
2015-02-28 17:36:37 -0800

    attempt at handling nfc

--- app/src/main/java/com/example/jenniferdai/nfc/MainActivity.java
+++ app/src/main/java/com/example/jenniferdai/nfc/MainActivity.java
@@ -1,19 +1,112 @@
 package com.example.jenniferdai.nfc;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    // Gets default NfcAdapter for the device
+    NfcAdapter mNfcAdapter;
+    // token that allows the foreign application to use your application's permissions to
+    // execute a predefined piece of code.
+    PendingIntent mNfcPendingIntent;
+    // declares the capabilities of its parent component — what an activity or service can do
+    // and what types of broadcasts a receiver can handle. It opens the component to receiving
+    // intents of the advertised type, while filtering out those that are not meaningful for
+    // the component.
+    IntentFilter[] mWriteTagFilters;
+    IntentFilter[] mNdefExchangeFilters;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
         setContentView(R.layout.activity_main);
+        // get widgets that you want to interact with
+        View.OnClickListener mTagWriter;
+        findViewById(R.id.write_tag).setOnClickListener(mTagWriter);
+        mNote = ((EditText) findViewById(R.id.note));
+        mNote.addTextChangedListener(mTextWatcher);
+
+        // Handle all of our received NFC intents in this activity.
+        mNfcPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        // Intent filters for reading a note from a tag or exchanging over p2p.
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            onResume();
+        } catch (IntentFilter.MalformedMimeTypeException e) { }
+        mNdefExchangeFilters = new IntentFilter[] { ndefDetected };
+
+        // Intent filters for writing to a tag
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        mWriteTagFilters = new IntentFilter[] { tagDetected };
     }
 
+    public void onResume() {
+        super.onResume();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Intent intent;
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                NdefMessage[] msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+        }
+        //process the msgs array
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mResumed = false;
+        mNfcAdapter.disableForegroundNdefPush(this);
+    }
+
+    NdefMessage[] getNdefMessages(Intent intent) {
+        // Parse the intent
+        NdefMessage[] msgs = null;
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                // Unknown tag type
+                byte[] empty = new byte[] {};
+                NdefRecord record =
+                        new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] {
+                        record
+                });
+                msgs = new NdefMessage[] {
+                        msg
+                };
+            }
+        } else {
+            Log.d(TAG, "Unknown intent.");
+            finish();
+        }
+        return msgs;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
