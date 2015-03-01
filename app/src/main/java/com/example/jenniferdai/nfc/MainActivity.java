d15ba104a034da71c4e6beef8edf41fd319881b1
Jennifer Dai <jennifah.hai.dai@gmail.com>
2015-02-28 18:30:00 -0800

    adds onNewIntent and calls

--- app/src/main/java/com/example/jenniferdai/nfc/MainActivity.java
+++ app/src/main/java/com/example/jenniferdai/nfc/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.jenniferdai.nfc;
 
+import android.app.AlertDialog;
 import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.IOException;
+import java.text.BreakIterator;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -33,10 +43,8 @@ public class MainActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_main);
         // get widgets that you want to interact with
-        View.OnClickListener mTagWriter;
-        findViewById(R.id.write_tag).setOnClickListener(mTagWriter);
-        mNote = ((EditText) findViewById(R.id.note));
-        mNote.addTextChangedListener(mTextWatcher);
+        EditText mNote = ((EditText) findViewById(R.id.note));
+        mNote.setText("new text");
 
         // Handle all of our received NFC intents in this activity.
         mNfcPendingIntent = PendingIntent.getActivity(this, 0,
@@ -45,13 +53,101 @@ public class MainActivity extends ActionBarActivity {
         // Intent filters for reading a note from a tag or exchanging over p2p.
         IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
         try {
-            onResume();
+            onNewIntent(mNfcPendingIntent);
         } catch (IntentFilter.MalformedMimeTypeException e) { }
         mNdefExchangeFilters = new IntentFilter[] { ndefDetected };
 
         // Intent filters for writing to a tag
         IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
         mWriteTagFilters = new IntentFilter[] { tagDetected };
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // NDEF exchange mode
+        boolean mWriteMode;
+        if (!mWriteMode && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            NdefMessage[] msgs = getNdefMessages(intent);
+            promptForContent(msgs[0]);
+        }
+
+        // Tag writing mode
+        if (mWriteMode && NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            writeTag(getNoteAsNdef(), detectedTag);
+        }
+    }
+
+    private NdefMessage getNoteAsNdef() {
+        BreakIterator mNote;
+        byte[] textBytes = mNote.getText().toString().getBytes();
+        NdefRecord textRecord = new NdefRecord(NdefRecord.TNF_MIME_MEDIA, "text/plain".getBytes(),
+                new byte[] {}, textBytes);
+        return new NdefMessage(new NdefRecord[] {
+                textRecord
+        });
+    }
+
+    boolean writeTag(NdefMessage message, Tag tag) {
+        int size = message.toByteArray().length;
+
+        try {
+            Ndef ndef = Ndef.get(tag);
+            if (ndef != null) {
+                ndef.connect();
+
+                if (!ndef.isWritable()) {
+                    toast("Tag is read-only.");
+                    return false;
+                }
+                if (ndef.getMaxSize() < size) {
+                    toast("Tag capacity is " + ndef.getMaxSize() + " bytes, message is " + size
+                            + " bytes.");
+                    return false;
+                }
+
+                ndef.writeNdefMessage(message);
+                toast("Wrote message to pre-formatted tag.");
+                return true;
+            } else {
+                NdefFormatable format = NdefFormatable.get(tag);
+                if (format != null) {
+                    try {
+                        format.connect();
+                        format.format(message);
+                        toast("Formatted tag and wrote message");
+                        return true;
+                    } catch (IOException e) {
+                        toast("Failed to format tag.");
+                        return false;
+                    }
+                } else {
+                    toast("Tag doesn't support NDEF.");
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            toast("Failed to write tag");
+        }
+
+        return false;
+    }
+
+    private void promptForContent(final NdefMessage msg) {
+        new AlertDialog.Builder(this).setTitle("Replace current content?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        String body = new String(msg.getRecords()[0].getPayload());
+                        setNoteBody(body);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                }).show();
     }
 
     public void onResume() {
@@ -102,7 +198,7 @@ public class MainActivity extends ActionBarActivity {
                 };
             }
         } else {
-            Log.d(TAG, "Unknown intent.");
+            //Log.d(TAG, "Unknown intent.");
             finish();
         }
         return msgs;
